eb7cbf2dbd1bdcd11bd96b1ec6d275f6da48586a
Electrocaruzo <gongrossman@gmail.com>
2026-01-07 16:04:06 +0200

    started adding chemistry utlilities thing

---
+++ chemistry_equations_utilities.js
@@ -0,0 +1,12 @@
+class Element {
+    elementSymbol;
+    elementName;
+    atomicNumber;
+    atomicWeight;
+    constructor(elementSymbol, elementName, atomicNumber, atomicWeight) {
+        this.elementSymbol = elementSymbol;
+        this.elementName = elementName;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight
+    }
+}
